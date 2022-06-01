886ed287e888e45d0df0d6fdee97e1357ff24785
root <rlgnswk12@gmail.com>
2022-06-01 19:09:17 +0000

    Selfref

--- train_selfRef.py
+++ train_selfRef.py
@@ -15,7 +15,7 @@ import os
 
 from torchinfo import summary
 
-import models
+import models_selfRef as models
 import utils
 import data_load
 #input sample of size 69 × 240
@@ -34,24 +34,6 @@ parser.add_argument('--batchSize', type=int, default=80, help='input batch size 
 parser.add_argument('--lr', type=float, default=0.001, help='learning rate')
 args = parser.parse_args()
 
-def calc_mean_std( feat, eps=1e-5):
-    # eps is a small value added to the variance to avoid divide-by-zero.
-    size = feat.size()
-    assert (len(size) == 4)
-    N, C = size[:2]
-    feat_var = feat.view(N, C, -1).var(dim=2) + eps
-    feat_std = feat_var.sqrt().view(N, C, 1, 1)
-    feat_mean = feat.view(N, C, -1).mean(dim=2).view(N, C, 1, 1)
-    return feat_mean, feat_std
-
-def calc_style_loss(out_feat_list, style_feat_list, style_loss_function):
-    assert len(out_feat_list) == len(style_feat_list)
-    style_loss= 0
-    for i in range(len(out_feat_list)):
-        out_mean, out_std = calc_mean_std(out_feat_list[i])
-        style_mean, stlye_std = calc_mean_std(style_feat_list[i])
-        style_loss += (style_loss_function(out_mean, style_mean) + style_loss_function(out_std, stlye_std))
-    return style_loss
 
 def main(args):
     
@@ -65,17 +47,15 @@ def main(args):
     
     
     if args.model_type == 'Up':
-        model = models.Convolutional_AE_AdaIN_Upsample().to(device)
+        model = models.Convolutional_AE_SelfRef().to(device)
     else:
-        model = models.Convolutional_AE_AdaIN().to(device)
+        model = models.Convolutional_AE_SelfRef().to(device)
     
     
     #load pretrained Encoder weight only 
     #torch.load_dict(pre_model.Encoder)
-    
-    
     saveUtils.save_log(str(args))
-    #saveUtils.save_log(str(summary(model, (1,1,69,240))))
+    saveUtils.save_log(str(summary(model, ((1,1,69,240), (1,1,69,240)))))
     
     train_dataloader, train_dataset = data_load.get_dataloader(args.datasetPath , args.batchSize, IsNoise=False, \
                                                                             IsTrain=True, dataset_mean=None, dataset_std=None)
@@ -99,10 +79,12 @@ def main(args):
     for num_epoch in range(args.numEpoch):
         
         total_loss = 0
+        total_train_recon_loss = 0
         total_train_content_loss = 0
         total_train_style_loss = 0
         
         total_v_loss = 0
+        total_v_recon_loss = 0
         total_v_content_loss = 0
         total_v_style_loss = 0
         
@@ -119,21 +101,38 @@ def main(args):
         for iter, item in enumerate(zip(train_dataloader,train_style_dataloader)):
             print_num +=1
             content_item, style_item = item
-            masked_content_input, gt_image = content_item
-            _, style_input = style_item
-            masked_content_input = masked_content_input.to(device, dtype=torch.float)
-            gt_image = gt_image.to(device, dtype=torch.float)
-            style_input = style_input.to(device, dtype=torch.float)
+            _, motion_a = content_item
+            _, motion_b = style_item
+            #masked_content_input = masked_content_input.to(device, dtype=torch.float)
+            motion_a = motion_a.to(device, dtype=torch.float)
+            motion_b = motion_b.to(device, dtype=torch.float)
             
-            _, out_latent, transfered_latent, out_feat_list, style_feat_list = model(masked_content_input, style_input)
+            out_style_B_Content_A, out_style_A_Content_B, content_latent_a, style_latent_b, content_latent_b, style_latent_a  = model(motion_a, motion_b)
             
-            style_loss = calc_style_loss(out_feat_list, style_feat_list, style_loss_function)
-            train_loss = loss_function(out_latent, transfered_latent)
+
+            out_style_A_Content_A, out_style_B_Content_B, content_latent_out_style_B_Content_A, style_latent_out_style_A_Content_B, content_latent_out_style_A_Content_B, style_latent_out_style_B_Content_A \
+                                                                                                                                                                = model(out_style_B_Content_A, out_style_A_Content_B)
+
+            reconstruction_loss_a = loss_function(motion_a, out_style_A_Content_A)
+            reconstruction_loss_b = loss_function(motion_b, out_style_B_Content_B)
             
-            total_train_loss = train_loss + 0.5 * style_loss 
+            loss_content_latent_a = loss_function(content_latent_a, content_latent_out_style_B_Content_A)
+            loss_content_latent_b = loss_function(content_latent_b, content_latent_out_style_A_Content_B)
+            
+            loss_style_latent_a = loss_function(style_latent_a, style_latent_out_style_A_Content_B)
+            loss_style_latent_b = loss_function(style_latent_b, style_latent_out_style_B_Content_A)
+
+
+            recon_total = reconstruction_loss_a + reconstruction_loss_b
+            content_total = loss_content_latent_a + loss_content_latent_b
+            style_total = loss_style_latent_a + loss_style_latent_b
+            
+            total_train_loss =  recon_total + content_total + style_total
+            
             total_loss += total_train_loss.item()
-            total_train_content_loss += train_loss.item()
-            total_train_style_loss += style_loss.item()
+            total_train_recon_loss += recon_total.item()
+            total_train_content_loss += content_total.item()
+            total_train_style_loss += style_total.item()
             
             optimizer.zero_grad()
             total_train_loss.backward()
@@ -142,16 +141,20 @@ def main(args):
             
             if iter % print_interval == 0 and iter != 0:
                 train_iter_loss =  total_loss * 0.01
+                total_train_recon_loss = total_train_recon_loss * 0.01
                 train_iter_content_loss = total_train_content_loss * 0.01
                 train_iter_style_loss = total_train_style_loss * 0.01
                 
-                log = "Train: [Epoch %d][Iter %d] [Total Train Loss: %.4f] [Content Train Loss: %.4f] [Style Train Loss: %.4f]" % (num_epoch, iter, train_iter_loss, train_iter_content_loss, train_iter_style_loss)
+                log = "Train: [Epoch %d][Iter %d] [Valid Loss: %.4f] [Recon Loss: %.4f] [Content Latent Loss: %.4f] [Style Latent Loss: %.4f]" %\
+                                             (num_epoch, iter, train_iter_loss, total_train_recon_loss, train_iter_content_loss, train_iter_style_loss)
                 print(log)
                 saveUtils.save_log(log)
                 writer.add_scalar("Train Total Loss/ iter", train_iter_loss, print_num)
+                writer.add_scalar("Train Recon Loss/ iter", total_train_recon_loss, print_num)
                 writer.add_scalar("Train Content Loss/ iter", train_iter_content_loss, print_num)
                 writer.add_scalar("Train Style Loss/ iter", train_iter_style_loss, print_num)
                 total_loss = 0
+                total_train_recon_loss = 0
                 total_train_content_loss = 0
                 total_train_style_loss = 0
                  
@@ -160,33 +163,53 @@ def main(args):
         for iter, item in enumerate(zip(valid_dataloader,valid_style_dataloader)):
             model.eval()
             content_item, style_item = item
-            masked_content_input, gt_image = content_item
-            _, style_input = style_item
-            masked_content_input = masked_content_input.to(device, dtype=torch.float)
-            gt_image = gt_image.to(device, dtype=torch.float)
-            style_input = style_input.to(device, dtype=torch.float)
+            _, motion_a = content_item
+            _, motion_b = style_item
+            #masked_content_input = masked_content_input.to(device, dtype=torch.float)
+            motion_a = motion_a.to(device, dtype=torch.float)
+            motion_b = motion_b.to(device, dtype=torch.float)
             
             with torch.no_grad():
-                out, out_latent, transfered_latent, out_feat_list, style_feat_list = model(masked_content_input, style_input)
-            style_val_loss = calc_style_loss(out_feat_list, style_feat_list, style_loss_function)
-            val_loss = loss_function(out_latent, transfered_latent)
+                out_style_B_Content_A, out_style_A_Content_B, content_latent_a, style_latent_b, content_latent_b, style_latent_a  = model(motion_a, motion_b)
             
-            total_val_loss = val_loss + 0.5 * style_val_loss
+                out_style_A_Content_A, out_style_B_Content_B, content_latent_out_style_B_Content_A, style_latent_out_style_A_Content_B, content_latent_out_style_A_Content_B, style_latent_out_style_B_Content_A \
+                                                                                                                                                                = model(out_style_B_Content_A, out_style_A_Content_B)
+            reconstruction_loss_a = loss_function(motion_a, out_style_A_Content_A)
+            reconstruction_loss_b = loss_function(motion_b, out_style_B_Content_B)
+            
+            loss_content_latent_a = loss_function(content_latent_a, content_latent_out_style_B_Content_A)
+            loss_content_latent_b = loss_function(content_latent_b, content_latent_out_style_A_Content_B)
+            
+            loss_style_latent_a = loss_function(style_latent_a, style_latent_out_style_A_Content_B)
+            loss_style_latent_b = loss_function(style_latent_b, style_latent_out_style_B_Content_A)
+            
+
+            recon_total = reconstruction_loss_a + reconstruction_loss_b
+            content_total = loss_content_latent_a + loss_content_latent_b
+            style_total = loss_style_latent_a + loss_style_latent_b
+            
+            total_val_loss =  recon_total + content_total + style_total
+            
             total_v_loss += total_val_loss.item()
-            total_v_content_loss += val_loss.item()
-            total_v_style_loss += style_val_loss.item()
+            total_v_recon_loss += recon_total.item()
+            total_v_content_loss += content_total.item()
+            total_v_style_loss += style_total.item()
+
             
             model.train()
 
         
         saveUtils.save_result(out, gt_image, masked_content_input, style_input, num_epoch)
         valid_epoch_loss = total_v_loss/len(valid_dataloader)
+        valid_epoch_recon_loss = total_v_recon_loss/len(valid_dataloader)
         valid_epoch_content_loss = total_v_content_loss/len(valid_dataloader)
         valid_epoch_style_loss = total_v_style_loss/len(valid_dataloader)
-        log = "Valid: [Epoch %d] [Valid Loss: %.4f] [Content Valid Loss: %.4f] [Style Valid Loss: %.4f]" % (num_epoch, valid_epoch_loss, valid_epoch_content_loss, valid_epoch_style_loss)
+        log = "Valid: [Epoch %d] [Valid Loss: %.4f] [Recon Loss: %.4f] [Content Latent Loss: %.4f] [Style Latent Loss: %.4f]" %\
+                                             (num_epoch, valid_epoch_loss, valid_epoch_recon_loss, valid_epoch_content_loss, valid_epoch_style_loss)
         print(log)
         saveUtils.save_log(log)
         writer.add_scalar("Valid Total Loss/ Epoch", valid_epoch_loss, num_epoch)
+        writer.add_scalar("Valid Recon Loss/ Epoch", valid_epoch_recon_loss, num_epoch) 
         writer.add_scalar("Valid Content Loss/ Epoch", valid_epoch_content_loss, num_epoch) 
         writer.add_scalar("Valid Style Loss/ Epoch", valid_epoch_style_loss, num_epoch) 
 
